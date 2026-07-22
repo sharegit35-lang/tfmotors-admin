3581fd168b15eceaa2a690bddb367a352cadadbd
Administrator <Administrator@tfmotors.com>
2026-07-22 15:03:39 +0700

    Update User model to support MongoDB connection

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,43 +4,32 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// 1. ប្តូរពី Authenticatable ធម្មតា មកប្រើ MongoDB Authenticatable វិញ
+use MongoDB\Laravel\Auth\User as Authenticatable; 
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // ១. បន្ថែម HasApiTokens សម្រាប់បង្កើត Token តាម API
-use Spatie\Permission\Traits\HasRoles; // មុខងារសិទ្ធិ (Roles) របស់អ្នក
+use Laravel\Sanctum\HasApiTokens; 
+use Spatie\Permission\Traits\HasRoles; 
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable, HasRoles; // ២. បន្ថែម HasApiTokens នៅទីនេះ
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // 2. កំណត់ Connection និង Collection សម្រាប់ MongoDB
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
     protected $fillable = [
         'name',
         'email',
         'password',
-        'department', // បន្ថែម department ដើម្បីឱ្យវាអាច save ចូល database បាន
+        'department',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
