d1b5e127c88cdaaa2172f18f7bec00b7cad3bb9f
Administrator <Administrator@tfmotors.com>
2026-07-22 15:07:27 +0700

    Update clean AuthController for MongoDB login

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// 1. ប្តូរពី Authenticatable ធម្មតា មកប្រើ MongoDB Authenticatable វិញ
-use MongoDB\Laravel\Auth\User as Authenticatable; 
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; 
-use Spatie\Permission\Traits\HasRoles; 
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    // 2. កំណត់ Connection និង Collection សម្រាប់ MongoDB
     protected $connection = 'mongodb';
     protected $collection = 'users';
 
