6f8c700be29986f4f79ed91be7397f9452a2d4da
Administrator <Administrator@tfmotors.com>
2026-07-22 15:20:59 +0700

    Fix User model for MongoDB Laravel package

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use MongoDB\Laravel\Auth\User as Authenticatable;
+// 1. ប្តូរមកប្រើ MongoDB Eloquent Model ធម្មតាវិញ
+use MongoDB\Laravel\Eloquent\Model; 
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+// 2. ប្រើប្រាស់ Authenticatable Contract របស់ Laravel ផ្ទាល់
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, Authenticatable;
 
     protected $connection = 'mongodb';
     protected $collection = 'users';
